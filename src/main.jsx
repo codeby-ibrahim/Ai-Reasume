c2275948bbfc17eacb20bb90ffc166d035731fb3
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-08-11 14:47:21 +0500

    added

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />
+      },
+      {
+        path: '/dashboard',
+        element: <dashboard />
       }
     ]
   },
