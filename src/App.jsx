c0d6f7810eb0c9e9d0af1de9ede5ebcbff2e7980
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-08-11 15:41:30 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useClerk } from '@clerk/clerk-react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const { user, isloaded, isSignedIn } = useClerk();
 
+  if (!isloaded) {
+    return Navigate('/auth/sign-in');
+  }
   return (
     <>
       <Outlet />
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import SigninPage from './auth/sign-in/index.jsx'
 import Home from './Home/index.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 
-
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 const router = createBrowserRouter([
   {
     element: <App />,
@@ -29,6 +30,8 @@ const router = createBrowserRouter([
 ])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
