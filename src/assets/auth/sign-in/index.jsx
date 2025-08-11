a0ff40efc347349efa47f7735f080a8de45e17ff
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-08-11 11:43:14 +0500

    added

---
+++ src/assets/auth/sign-in/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const signnPage = () => {
+    return (
+        <div>
+            <h1>Sign Page</h1>
+        </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: 'auth/signin'
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
